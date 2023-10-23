1cec2c5d7000b6f845ae0ba93f423d6c91c59a0f
jenish-varsani <jenish.varsani.ciyaza@gmail.com>
2023-10-23 10:52:56 +0530

    datatypes

---
+++ 01_basic/01_variables.js
@@ -0,0 +1,15 @@
+const accountId = 144553
+let accountEmail = "jenish.varsani@gmail.com"
+var accountPassword = "12345"
+accountCity = "Rajkot"
+let accountState;
+
+// accountId = 2 //not allowed
+
+accountEmail = "Hello@gmail.com"
+accountPassword = "54321"
+accountCity = "Bengaluru"
+
+console.log(accountId);
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
---
+++ 01_basic/02_dataType.js
@@ -0,0 +1,28 @@
+"Use strict"; //treat all js code as newer versions
+
+// alert(3 + 3); //we are using nodejs,not browser
+
+console.log(3 + 3) //code readability should br high
+console.log("Hello world!");
+
+
+let name ="Hello world!"
+let age = 18
+let isLoggedIn = false;
+let state;
+
+// primitive datetype
+
+// number => 2 to POWER 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined =>
+// symbol => unique
+
+
+// object
+
+console.log(typeof undefined); //undefined
+console.log(typeof null); //object
