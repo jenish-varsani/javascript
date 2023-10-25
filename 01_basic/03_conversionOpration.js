cf712b5b6353a7b53fc53362979038d476d23ef5
jenish-varsani <jenish.varsani.ciyaza@gmail.com>
2023-10-25 18:09:27 +0530

    modify

--- 01_basic/03_conversionOpration.js
+++ 01_basic/03_conversionOpration.js
@@ -22,5 +22,36 @@ let booleanIsLoggedIn = Boolean(isLoggedIn)
 
 let someNumber = 33
 let stringNumber = String(someNumber)
-console.log(someNumber);
-console.log(typeof stringNumber);
+// console.log(someNumber);
+// console.log(typeof stringNumber);
+
+// ***************************** Operation ***************************************
+
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2);
+// console.log(2/2);
+// console.log(2%2);
+
+let str1 = "hello"
+let str2 = " jenish"
+
+let str3 = str1 + str2
+console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+console.log((1 + 2) - "2");
+console.log(1 + 2 + "2");
+
+// console.log( (3 + 4) * 5 % 3);
+
+console.log(+true);
+console.log(+"");
